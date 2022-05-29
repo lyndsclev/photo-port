1ab782c5f1de06f94e2654165c15a6ff062ddf57
lyndsclev <lyndsclev@gmail.com>
2022-05-29 16:22:31 -0400

    added cover and nav

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+import Nav from './components/Nav/index'; 
+import About from './components/About/index'; 
+
+function App() {
+  return (
+    <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>    
+    </div>
+  );
+}
+
+export default App;

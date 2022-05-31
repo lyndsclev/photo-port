1e2a47b5f48ffcece0a6a8e52512dcec3437bcde
lyndsclev <lyndsclev@gmail.com>
2022-05-31 15:52:44 -0400

    useState added

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,8 +1,20 @@
-const Gallery = () => {
+import {capitalizeFirstLetter} from '../../utils/helpers'; 
+import photo from '../../assets/small/commercial/0.jpg'; 
+
+const Gallery = (props) => {
+
+    const currentCategory = {
+        name: "commercial",
+        description: "Photos of grocery stores, food trucks, and other commercial projects"
+    }; 
+
     return (
         <section>
-            <h1>NAME</h1>
-            <p>DESCRIPTION</p>
+            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+            <p>{currentCategory.description}</p>
+            <div className='flex-row'>
+                <img src={photo} alt="commerical example" className="img-thumbnail mx-1" />
+            </div>
         </section>
     );
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,9 @@
+import { useState } from 'react'; 
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
 const Nav = () => {
 
-    const categories = [
+    const [categories] = useState([
         {
             name: "commercial", 
             description: "Photos of grocery stores, food trucks, and other commercial projects"
@@ -17,23 +20,25 @@ const Nav = () => {
             name: "landscape", 
             description: "Fields, farmhouses, waterfalls, and the beauty of nature"
         }
-    ]; 
+    ]); 
 
-    const categorySelected = (name) => {
-        console.log(`${name} clicked`); 
-    }
+    const [currentCategory, setCurrentCategory] = useState(categories[0]); 
 
     return (
-        <header>
+        <header className='flex-row px-1'>
             <h2>
-                <a href="/">
-                    <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+                <a data-test-id='link' href='/'>
+                    <span role='img' aria-label='camera'>
+                        {" "}
+                        📸
+                    </span>{" "}
+                    Oh Snap! 
                 </a>
             </h2>
             <nav>
-                <ul className="flex-row">
-                    <li className="mx-2">
-                        <a href="#about">
+                <ul className='flex-row'>
+                    <li className='mx-2'>
+                        <a href='#about'>
                             About me
                         </a>
                     </li>
@@ -41,12 +46,15 @@ const Nav = () => {
                         <span>Contact</span>
                     </li>
                     {categories.map((category) => (
-                        <li 
-                        className="mx-1" 
-                        key={category.name}
-                        >
-                            <span onClick={() => categorySelected(category.name)}>
-                                {category.name}
+                        <li className={`mx-1 ${
+                            currentCategory.name === category.name && 'navActive'
+                        }`} key={category.name}>
+                            <span
+                                onClick={() => {
+                                    setCurrentCategory(category)
+                                }}
+                            >
+                                {capitalizeFirstLetter(category.name)}
                             </span>
                         </li>
                     ))}
